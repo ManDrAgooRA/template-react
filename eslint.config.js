4addcc1a31ef88e5e2afcdaee20474d39c6d893d
eugene <egorobey@lytvynov-production.com>
2024-12-26 09:36:10 +0200

    Init project

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "react/react-in-jsx-scope": "off",
+      "capitalized-comments": "error",
+    },
+  },
+];
