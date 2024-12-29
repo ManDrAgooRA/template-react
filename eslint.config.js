7cf61ad2ae5a5d761d53ad77fd857bdb4ce860f0
gorobey.evgeniy <zekagorobey@gmail.com>
2024-12-29 17:00:16 +0200

    feature: add aliases

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,44 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
+  { ignores: ["dist", "build", "node_modules", "eslint.config.js"] },
   {
+    extends: [
+      pluginJs.configs.recommended,
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      eslintPluginPrettier,
+    ],
+    files: ["src/*.{ts,tsx}"],
     settings: {
       react: {
         version: "detect",
       },
     },
-  },
-  {
-    ignores: ["dist", "build", "node_modules", "eslint.config.js"],
-  },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintPluginPrettier,
-  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       "no-unused-vars": "error",
       "no-undef": "error",
       "react/react-in-jsx-scope": "off",
       "capitalized-comments": "error",
     },
   },
-];
+);
